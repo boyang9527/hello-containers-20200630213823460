4f01705498f2ffdd43c76260083b869571266f0c
Bo Yang <byang@us.ibm.com>
2020-06-30 22:07:41 +0000

    demo

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service');
+  res.send('hello, this is a cloud shell demo');
 });
 
 app.listen(PORT);
-console.log(' Application Running on port' + PORT);
+console.log(' Application Running on port ' + PORT);
